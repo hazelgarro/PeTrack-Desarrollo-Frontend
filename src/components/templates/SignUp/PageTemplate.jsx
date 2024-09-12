8710bc802994b7f415fe95df24f3e1ba4957831c
hazelgarro <hazelvaleriagarroarias@gmail.com>
2024-09-12 15:11:22 -0600

    background added to the forms

--- src/components/templates/SignUp/PageTemplate.jsx
+++ src/components/templates/SignUp/PageTemplate.jsx
@@ -4,7 +4,7 @@ import logo from '../../../assets/img/logo.png';
 
 const PageTemplate = () => (
   <div
-    className="bg-[url(../../../assets/img/movil_Bg.png)] md:bg-[url(../../../assets/img/Bg.png)] bg-cover bg-no-repeat bg-center w-screen h-screen overflow-hidden"
+    className="bg-[url('/assets/img/movil_Bg.png')] md:bg-[url('/assets/img/Bg.png')] bg-cover bg-no-repeat bg-center w-screen h-screen overflow-hidden"
   >
     <div className="md:px-96">
       <div className="flex justify-center mt-20">
