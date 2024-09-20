dc79413481d6b39023171eb2906782f716b3db21
DerrickMC7 <145299180+DerrickMC7@users.noreply.github.com>
2024-09-20 16:38:02 -0600

    Export to work at home
    nothing important yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import './App.css';
 import './index.css';
-// import PasswordInput from './components/molecules/PasswordInput';
-import PetProfile from "./components/templates/Profile";
-import CardsContainer from "./components/organisms/cardsContainer"
+import  Login  from "./components/templates/LogIn/index.jsx";
 
-import pet_picture from "./assets/img/pet_picture.webp";
-
-/*
-<div>
+/*<div>
   <Routes>
     <Route path="/" element={<Login />} />
-    <Route path="/signup" element={<Login />} />
   </Routes>
-</div>
-*/
+</div>*/
 function App() {
   return (
-    <>
-      {/* <PetPhotoQr typeCard="pet" link="#" imgSrc={pet_picture} imgAlt="Pet image"></PetPhotoQr> */}
-      <PetProfile></PetProfile>
-    </>
+    <Login></Login>
   );
 }
 
