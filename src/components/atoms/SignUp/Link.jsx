30e199b367d4734c8d43bcb8c8181c29ee10fcb2
Daniel Ramirez <danxram@gmail.com>
2024-09-23 14:40:42 -0600

    Acomodar carpetas

--- src/components/atoms/SignUp/Link.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Link = ({ href, text, className }) => (
-  <a className={className} href={href}>
-    {text}
-  </a>
-);
-
-export default Link;
--- src/components/atoms/SignUp/Text.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Text = ({ text, className }) => (
-  <p className={className}>{text}</p>
-);
-
-export default Text;
