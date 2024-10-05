cb5f30a12fc2f2bd7cc7096d726ca24e5a975e41
Brandon Josué Brenes <brandonjosuebrenes@gmail.com>
2024-10-05 02:32:27 -0600

    Se agregaron las funciones para subir imagenes, eliminar imagenes, reemplazarlas y obtener url con tamaños específicos de las imagenes en cloudinary

--- src/components/atoms/Icons/MenuHamburger/index.jsx
+++ src/components/atoms/Icons/MenuHamburger/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import "../styles.css";
 
-export default function MenuHamburgerIcon({ size = 'medium', color = 'primary', thickness = '1.5', ...props }) {
+export default function MenuHamburger({ size = 'medium', color = 'primary', thickness = '1.5', ...props }) {
     return (
         <svg className={`svg-icon svg-icon--${size} svg-icon--${color}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} fill="none" {...props}>
             <path d="M4 5L20 5" stroke="currentColor" strokeWidth={thickness} strokeLinecap="round" strokeLinejoin="round" />
@@ -11,7 +11,7 @@ export default function MenuHamburgerIcon({ size = 'medium', color = 'primary', 
     );
 }
 
-MenuHamburgerIcon.propTypes = {
+MenuHamburger.propTypes = {
     size: PropTypes.oneOf(["extra-small", "small", "medium", "large"]),
     color: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
     thickness: PropTypes.string, // Width of the icon's stroke
--- src/utils/imageManager.js
+++ src/utils/imageManager.js
@@ -1,9 +1,105 @@
-export function uploadImage(image){
-    return(
-        {
-            result: true,
-            imageUrl: "",
-            message: "An error occurred while creating the account."
+import { v4 as uuidv4 } from 'uuid';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'dg2l6tblc',
+    api_key: '915636439722653',
+    api_secret: 'QCQ_ubacuA0wCrgfp9KllJMdNkg'
+});
+
+async function uploadToCloudinary(image, publicId, options = {}) {
+    try {
+        const uploadResult = await cloudinary.uploader.upload(image, {
+            public_id: publicId,
+            format: options.format || 'webp', // Permite cambiar el formato a través de opciones
+            ...options
+        });
+
+        return uploadResult;
+    } catch (error) {
+        console.error("Error uploading image:", error);
+        throw new Error(`Failed to upload image: ${error.message}`);
+    }
+}
+
+export async function uploadImage(image, folder) {
+    try {
+        const generatedPublicId = `${folder}/picture_${uuidv4()}`;
+        const uploadResult = await uploadToCloudinary(image, generatedPublicId);
+
+        const optimizedUrl = cloudinary.url(generatedPublicId, {
+            fetch_format: 'auto',
+            quality: 'auto'
+        });
+
+        return {
+            result: uploadResult,
+            message: "Image uploaded successfully",
+            uploadedImage: {
+                imageUrl: optimizedUrl,
+                publicId: generatedPublicId
+            }
+        };
+
+    } catch (error) {
+        console.error("Error uploading image:", error);
+        throw new Error(`Failed to upload image: ${error.message}`);
+    }
+}
+
+//Devuelve una url con una imagen reducida, esto para agilizar cargas en determinados casos
+export async function getCroppedUrl(publicId, width, height) {
+    try {
+        const croppedUrl = cloudinary.url(publicId, {
+            crop: 'auto',
+            gravity: 'auto',
+            width: width,
+            height: height
+        });
+
+        return croppedUrl;
+    } catch (error) {
+        console.error("Error getting cropped URL:", error);
+        throw new Error(`Failed to get cropped URL: ${error.message}`);
+    }
+}
+
+export async function deleteImage(publicId) {
+    try {
+        if (!publicId) {
+            throw new Error('No public_id provided for deletion.');
         }
-    );
+
+        const response = await cloudinary.uploader.destroy(publicId);
+
+        if (response.result === 'ok') {
+            console.log(`Image deleted successfully.`);
+            return { result: true, message: 'Image deleted successfully' };
+        } else {
+            console.error(`Error deleting image: ${response.result}`);
+            return { result: false, message: 'Error deleting the image' };
+        }
+    } catch (error) {
+        console.error('Error attempting to delete the image:', error);
+        return { result: false, message: error.message };
+    }
+}
+
+export async function replaceImage(imagePath, publicId) {
+    try {
+        if (!publicId) {
+            throw new Error('No public_id provided for replacing the image.');
+        }
+
+        const uploadResult = await uploadToCloudinary(imagePath, publicId, {
+            overwrite: true,
+            format: 'webp',
+        });
+        
+        console.log('Image replaced successfully:', uploadResult);
+        return { result: true, message: 'Image replaced successfully' };
+    } catch (error) {
+        console.error('Error replacing the image:', error);
+        throw new Error(`Failed to replace image: ${error.message}`); // Mensaje de error más informativo
+    }
 }
