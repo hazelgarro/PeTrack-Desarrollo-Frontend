fb91c7f206c5d6cb156f5423f5c575a7766e23b1
Daniel Ramirez <danxram@gmail.com>
2024-10-10 22:59:51 -0600

    HomePage Responsive

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -2,7 +2,6 @@ import Nav from "../../organisms/Nav";
 import Pet from "../../../assets/img/pet_picture.webp";
 
 import Welcome from "../../organisms/WelcomeContainer";
-import ServicesContainer from "../../organisms/ServicesContainer";
 import CardsContainer from "../../organisms/cardsContainer";
 import Card from "../../molecules/Card";
 import Button from "../../atoms/Button";
@@ -13,26 +12,25 @@ export default function HomePage() {
             <Nav></Nav>
             <Welcome></Welcome>
             <div className="flex justify-center items-center pt-16">
-                <p className="text-5xl ml-14 font-medium text-petrack-green">Mis Mascotas</p>
+                <p className="text-3xl md:text-5xl ml-14 font-medium text-petrack-green text-center">Mis Mascotas</p>
             </div>
-            <div className="mx-44 my-20">
+            <div className="mx-12 sm:mx-24 md:mx-44 my-20">
                 <CardsContainer>
                     <Card typeCard="pet" imgSrc={Pet} imgAlt="MedicalRecord" ></Card>
                     <Card typeCard="pet" imgSrc={Pet} imgAlt="MedicalRecord" ></Card>
                     <Card typeCard="pet" imgSrc={Pet} imgAlt="MedicalRecord" ></Card>
                 </CardsContainer>
             </div>
-            <ServicesContainer></ServicesContainer>
-            <div className="grid gap-10 py-24">
-                <h2 className="text-8xl font-bold text-petrack-green text-center">Cambia una vida</h2>
-                <p className="text-4xl font-semibold text-center">Adopta una mascota</p>
-                <div className=" flex justify-center items-center gap-8">
+            <div className="grid gap-5 md:gap-10 pt-24">
+                <h2 className="text-3xl md:text-8xl font-bold text-petrack-green text-center">Cambia una vida</h2>
+                <p className="text-xl md:text-4xl font-semibold text-center">Adopta una mascota</p>
+                <div className=" flex flex-col md:flex-row justify-center items-center gap-3 md:gap-8">
                     <Button type="button" size="small" variant="solid-green">Todos</Button>
                     <Button type="button" size="small" variant="border-green">Perros</Button>
                     <Button type="button" size="small" variant="border-green">Gatos</Button>
                 </div>
             </div>
-            <div className="mx-44 mb-10">
+            <div className="mx-12 md:mx-24 lg:mx-44 my-8 md:my-20">
                 <CardsContainer>
                     <Card typeCard="adoption_pet" imgSrc={Pet} imgAlt="MedicalRecord" ></Card>
                     <Card typeCard="adoption_pet" imgSrc={Pet} imgAlt="MedicalRecord" ></Card>
