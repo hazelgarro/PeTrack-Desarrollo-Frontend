90159f6cb594c0e952c3503b2db6082e2058d661
Brandon Josué Brenes <brandonjosuebrenes@gmail.com>
2024-11-01 12:26:44 -0600

    Se agregó la funcionalidad para restablecer contraseña

--- src/components/pages/ResetPassword/index.jsx
+++ src/components/pages/ResetPassword/index.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import TextInput from "../../atoms/TextInput";
+import { useNavigate, useLocation } from 'react-router-dom';
 import PasswordInput from "../../molecules/PasswordInput";
 import Button from "../../atoms/Button";
 import Form from "../../organisms/Form";
 import AccountForm from "../../templates/AccountForm";
 import Loader from "../../atoms/Loader";
-import {loginUser} from "../../../utils/sessionManager";
-import { useSession } from '../../../context/SessionContext';
+import { getData } from "../../../utils/apiConnector.js";
 
 export default function ResetPassword() {
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const token = queryParams.get("token");
+
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
@@ -20,75 +22,79 @@ export default function ResetPassword() {
     });
 
     const handleInputChange = ({ name, value }) => {
-        setPasswords({
-            ...passwords,
-            [name]: value
+        setPasswords((prevData) => {
+            const newData = {
+                ...prevData,
+                [name]: value
+            };
+
+            if (newData.password === newData.confirmPassword) {
+                setErrorMessage("");
+            } else {
+                setErrorMessage("Las contraseñas no coinciden");
+            }
+
+            return newData;
         });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        console.log(token);
+
+        if (passwords.newPassword !== passwords.confirmPassword) {
+            return;
+        } else if (passwords.newPassword.length < 8) {
+            setErrorMessage("La contraseña debe de tener mínimo 8 caracteres");
+            return;
+        }
+
         setIsLoading(true);
 
-        // try {
-        //     const apiResponse = await loginUser(
-        //         passwords.email,
-        //         passwords.password
-        //     );
-
-        //     setIsLoading(false);
-
-        //     if (apiResponse.result) {
-        //         let message;
-        //         if (apiResponse.data.userTypeId === "O") {
-        //             message = "Bienvenid@ " + apiResponse.data.details.completeName;
-        //         } else {
-        //             message = "Bienvenidos " + apiResponse.data.details.name;
-        //         }
-        //         updateSessionState();
-        //         alert(JSON.stringify(message));
-        //         navigate("/Homepage");
-        //     } else {
-        //         setErrorMessage(apiResponse.message);
-        //         setTimeout(() => {
-        //             setErrorMessage(null);
-        //         }, 7000);
-        //     }
-
-        // } catch (error) {
-        //     alert("Login error: " + error.message);
-        //     console.error("Error:", error);
-        // } finally {
-        //     setIsLoading(false);
-        // }
-
         try {
-            
+            const apiUrl = "https://www.APIPetrack.somee.com/User/ResetPassword";
+            const body = {newPassword : passwords.newPassword, confirmPassword : passwords.confirmPassword, token: token};
+            const apiResponse = await getData(apiUrl, body, false, "POST");
+
+            setIsLoading(false);
+
+            if(apiResponse.result){
+                alert("La contreseña ha sido cambiada exitosamente");
+                navigate("/Login")
+            }else {
+                alert(apiResponse.message);
+            }
+
         } catch (error) {
-            
+            alert("Error al cambiar la contraseña: " + error.message);
+            console.error("Error:", error);
+        } finally {
+            setIsLoading(false);
         }
     };
 
     return (
         <AccountForm>
             {isLoading && <Loader />} {/* Muestra el loader mientras se carga */}
-            <Form title="Cambiar contraseña" subTitle="Por favor ingresa una nueva contraseña" onSubmit={handleSubmit}>
+            <Form title="Cambiar contraseña" subTitle="Por favor ingresa una nueva contraseña." onSubmit={handleSubmit}>
                 <PasswordInput
                     size="medium"
                     placeholder="New password"
-                    name="password"
-                    value={passwords.password}
+                    name="newPassword"
+                    value={passwords.newPassword}
                     onChange={handleInputChange}
                 />
                 <PasswordInput
                     size="medium"
                     placeholder="Confirm"
-                    name="password"
+                    name="confirmPassword"
                     value={passwords.confirmPassword}
                     onChange={handleInputChange}
                 />
-                {errorMessage && <p className="m-1 text-red-500 text-petrack-yellow">{errorMessage}</p>}
+                {errorMessage && <p className="m-1 w-full text-red-500 text-petrack-yellow">{errorMessage}</p>}
 
+                <a href="/AccountRecovery" className="text-right text-sm text-gray-500 mb-2">¿Tu token ha expirado?</a>
                 <Button type="submit" size="small" variant="solid-green">Cambiar contraseña</Button>
             </Form>
         </AccountForm>
